554bf493f1e26f6d1d5e2c553f020385528fd4ea
Ayubxon Obidov <128991489+ayubxon0880@users.noreply.github.com>
2025-04-30 18:50:04 +0500

    api changed

--- src/service/fileService.jsx
+++ src/service/fileService.jsx
@@ -6,16 +6,13 @@ const uploadFile = async (file,data) => {
     formData.append('formData', data);
 
     try {
-        const response = await axios.post('http://localhost:8081/api/v1/files', formData, {
+        const response = await axios.post('https://qaxvachi.uz/api/v1/files', formData, {
             headers: {
                 'Content-Type': 'multipart/form-data',
             },
         });
-
-        // Success response
         return { success: true, data: response.data };
     } catch (error) {
-        // Error response
         if (error.response) {
             if (error.response.status === 429) {
                 return { success: false, message: 'You have exceeded your daily limit of file uploads.' };
