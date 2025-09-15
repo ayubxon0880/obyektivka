4179fbf339d2a6b5244b13b0e0bc3ea69a52717a
Ayubxon Obidov <ayubxonnt@gmail.com>
2025-09-15 14:45:22 +0500

    analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
-// import {useEffect, useState} from "react";
-// import canUpload from "./service/canUpload.jsx";
-// import Navbar from "./component/Navbar.jsx";
-// import Home from "./pages/Home.jsx";
 import { Route, Routes } from "react-router-dom";
 import ResumeForm from "./pages/ResumeForm.jsx";
 import Footer from "./pages/Footer.jsx";
 import Portfolio from "./pages/Portfolio.jsx";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
-    // const [error, setError] = useState(null);
 
-    // useEffect(() => {
-    //     canUpload().then((res) => {
-    //         if (!res.success) {
-    //             setError(res.message);
-    //         }
-    //     }).catch((err) => {
-    //         setError(err);
-    //     });
-    // }, []);
 
     return (
         <div className="min-h-screen bg-gray-50">
             {/* <Navbar/> */}
-
+            <Analytics/>
             <div className="py-8 px-4">
               <Routes>
                     {/*<Route path="/" element={<Portfolio />} />*/}
