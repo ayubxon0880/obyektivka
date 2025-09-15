2b06461abb15185c5c058188797aaa024a2628cf
Ayubxon Obidov <ayubxonnt@gmail.com>
2025-09-15 14:49:00 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import ResumeForm from "./pages/ResumeForm.jsx";
 import Footer from "./pages/Footer.jsx";
-import Portfolio from "./pages/Portfolio.jsx";
 import { Analytics } from "@vercel/analytics/react"
 
 function App() {
@@ -9,13 +8,10 @@ function App() {
 
     return (
         <div className="min-h-screen bg-gray-50">
-            {/* <Navbar/> */}
             <Analytics/>
             <div className="py-8 px-4">
               <Routes>
-                    {/*<Route path="/" element={<Portfolio />} />*/}
                   <Route path="/" element={<ResumeForm />} />
-                  <Route path="/profile/*" element={<Portfolio />} />
               </Routes>
             </div>
 
--- src/env.jsx
+++
@@ -1,2 +0,0 @@
-export const API = "https://qaxvachi.uz/api/v1";
-// export const API = "http://localhost:8081/api/v1";
